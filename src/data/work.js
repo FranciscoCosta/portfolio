ac05da4ec6adce0581b171ba6607fe3380208468
Xico <francisco100eg@gmail.com>
2023-03-10 18:07:54 -0300

    Feat: Added new work

--- src/data/work.js
+++ src/data/work.js
@@ -1,57 +1,67 @@
-import work1 from '../assets/work1.png';
-import work2 from '../assets/work2.png';
-import work3 from '../assets/work3.png';
-import work4 from '../assets/work4.png';
-import work5 from '../assets/work5.png';
+import work1 from "../assets/work1.png";
+import work2 from "../assets/work2.png";
+import work3 from "../assets/work3.png";
+import work4 from "../assets/work4.png";
+import work5 from "../assets/work5.png";
+import visualVortex from "../assets/virsualvortex";
 
+const work = [
+  {
+    name: "VisualVortex",
+    img: visualVortex,
+    description: "AI generator App",
+    projectLink: "https://virtualvortex.netlify.app/",
+    codeLink: "https://github.com/FranciscoCosta/VisualVortex",
+    title: "VisualVortex",
+    tags: ["React JS", "Web App", "AI"],
+  },
 
-const work = [{
-    name: "Português",
+  {
+    name: "Restaurante",
     img: work1,
-    description:"Restaurante landing page",
+    description: "Restaurante landing page",
     projectLink: "https://portugueslp.netlify.app/",
     codeLink: "https://github.com/FranciscoCosta/RestauranteLP",
     title: "Português LP",
-    tags: ["UI/UX","React JS","Web App"],
-},
-{
+    tags: ["UI/UX", "React JS", "Web App"],
+  },
+  {
     name: "Game TrybeWars",
     img: work2,
-    description:"Um jogo de browser feito com React, Mysql, Express e Node",
+    description: "Um jogo de browser feito com React, Mysql, Express e Node",
     projectLink: "https://github.com/Alunos-Trybe/TrybeWars",
     codeLink: "https://github.com/Alunos-Trybe/TrybeWars",
     title: "TrybeWars",
-    tags: ["Desenvolvimento","React JS"],
-},
-{
+    tags: ["Desenvolvimento", "React JS"],
+  },
+  {
     name: "T-Recipes",
     img: work3,
-    description:"Uma app na qual a pessoa pode pesquisar receitas de comida e bebida",
+    description:
+      "Uma app na qual a pessoa pode pesquisar receitas de comida e bebida",
     projectLink: "https://t-recipe.netlify.app/meals",
     codeLink: "https://github.com/FranciscoCosta/app-receitas-grupo-20",
     title: "T-Recipes",
-    tags: ["Web App","React JS"],
-},
-{
+    tags: ["Web App", "React JS"],
+  },
+  {
     name: "Trybe Market",
     img: work4,
-    description:"Um e-commerce criado através da api do mercado livre",
+    description: "Um e-commerce criado através da api do mercado livre",
     projectLink: "https://lnkd.in/dBNHNzBr",
     codeLink: "https://lnkd.in/dQYWMh-q",
     title: "Trybe Market",
-    tags: ["UI/UX","React JS","Web App"],
-},
-{
+    tags: ["UI/UX", "React JS", "Web App"],
+  },
+  {
     name: "Trivia",
     img: work5,
-    description:"Um jogo de perguntas e respostas",
+    description: "Um jogo de perguntas e respostas",
     projectLink: "https://trivia-trybe-game.netlify.app/",
     codeLink: "https://github.com/FranciscoCosta/triviatrybe",
     title: "Trivia",
-    tags: ["React JS","Web App"],
-},
-
-]
-
+    tags: ["React JS", "Web App"],
+  },
+];
 
 export default work;
